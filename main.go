99231c3e69d81fb414d15042d0c5e19737493db2
kaleabbyh <kaleabbyh@gmail.com>
2023-12-02 08:38:42 +0300

    main.go update(validator)

--- main.go
+++ main.go
@@ -481,87 +481,129 @@
 // 	e.Start(":1323")
 // }
 
+// package main
+
+// import (
+// 	"net/http"
+// 	"strconv"
+// 	"sync"
+// 	"time"
+
+// 	"github.com/labstack/echo/v4"
+// )
+
+// type (
+// 	Stats struct {
+// 		Uptime       time.Time      `json:"uptime"`
+// 		RequestCount uint64         `json:"requestCount"`
+// 		Statuses     map[string]int `json:"statuses"`
+// 		mutex        sync.RWMutex
+// 	}
+
+// )
+
+// func NewStats() *Stats {
+// 	return &Stats{
+// 		Uptime:   time.Now(),
+// 		Statuses: map[string]int{},
+// 	}
+// }
+
+// // Process is the middleware function.
+// func (s *Stats) Process(next echo.HandlerFunc) echo.HandlerFunc {
+// 	return func(c echo.Context) error {
+// 		if err := next(c); err != nil {
+// 			c.Error(err)
+// 		}
+// 		s.mutex.Lock()
+// 		defer s.mutex.Unlock()
+// 		s.RequestCount++
+// 		status := strconv.Itoa(c.Response().Status)
+// 		s.Statuses[status]++
+// 		return nil
+// 	}
+// }
+
+// // Handle is the endpoint to get stats.
+// func (s *Stats) Handle(c echo.Context) error {
+// 	s.mutex.RLock()
+// 	defer s.mutex.RUnlock()
+// 	return c.JSON(http.StatusOK, s)
+// }
+
+// // ServerHeader middleware adds a `Server` header to the response.
+// func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+// 	return func(c echo.Context) error {
+// 		c.Response().Header().Set(echo.HeaderServer, "Echo/3.0")
+// 		return next(c)
+// 	}
+// }
+
+// func main() {
+// 	e := echo.New()
+
+// 	// Debug mode
+// 	e.Debug = true
+
+// 	//-------------------
+// 	// Custom middleware
+// 	//-------------------
+// 	// Stats
+// 	s := NewStats()
+// 	e.Use(s.Process)
+// 	e.GET("/stats", s.Handle) // Endpoint to get stats
+
+// 	// Server header
+// 	e.Use(ServerHeader)
+
+// 	// Handler
+// 	e.GET("/", func(c echo.Context) error {
+// 		return c.String(http.StatusOK, "Hello, World!")
+// 	})
+
+// 	// Start server
+// 	e.Logger.Fatal(e.Start(":1323"))
+// }
+
 package main
 
 import (
 	"net/http"
-	"strconv"
-	"sync"
-	"time"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 )
 
 type (
-	Stats struct {
-		Uptime       time.Time      `json:"uptime"`
-		RequestCount uint64         `json:"requestCount"`
-		Statuses     map[string]int `json:"statuses"`
-		mutex        sync.RWMutex
-	}
-	
+  User struct {
+    Name  string `json:"name" validate:"required"`
+    Email string `json:"email" validate:"required,email"`
+  }
+
+  CustomValidator struct {
+    validator *validator.Validate
+  }
 )
 
-
-func NewStats() *Stats {
-	return &Stats{
-		Uptime:   time.Now(),
-		Statuses: map[string]int{},
-	}
-}
-
-// Process is the middleware function.
-func (s *Stats) Process(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if err := next(c); err != nil {
-			c.Error(err)
-		}
-		s.mutex.Lock()
-		defer s.mutex.Unlock()
-		s.RequestCount++
-		status := strconv.Itoa(c.Response().Status)
-		s.Statuses[status]++
-		return nil
-	}
-}
-
-// Handle is the endpoint to get stats.
-func (s *Stats) Handle(c echo.Context) error {
-	s.mutex.RLock()
-	defer s.mutex.RUnlock()
-	return c.JSON(http.StatusOK, s)
-}
-
-// ServerHeader middleware adds a `Server` header to the response.
-func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		c.Response().Header().Set(echo.HeaderServer, "Echo/3.0")
-		return next(c)
-	}
+func (cv *CustomValidator) Validate(i interface{}) error {
+  if err := cv.validator.Struct(i); err != nil {
+       return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+  }
+  return nil
 }
 
 func main() {
-	e := echo.New()
-
-	// Debug mode
-	e.Debug = true
-
-	//-------------------
-	// Custom middleware
-	//-------------------
-	// Stats
-	s := NewStats()
-	e.Use(s.Process)
-	e.GET("/stats", s.Handle) // Endpoint to get stats
-
-	// Server header
-	e.Use(ServerHeader)
-
-	// Handler
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+  e := echo.New()
+  e.Validator = &CustomValidator{validator: validator.New()}
+  e.POST("/users", func(c echo.Context) (err error) {
+    u := new(User)
+    if err = c.Bind(u); err != nil {
+      return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+    }
+    if err = c.Validate(u); err != nil {
+      return err
+    }
+    return c.JSON(http.StatusOK, u)
+  })
+  e.Logger.Fatal(e.Start(":1323"))
 }
